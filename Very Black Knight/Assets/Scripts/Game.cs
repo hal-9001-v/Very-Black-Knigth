af7551ecd7bc4c17c02d9a540d5746187f24c790
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-02-13 21:01:47 +0100

    Comments to code

--- Very Black Knight/Assets/Scripts/Game.cs
+++ Very Black Knight/Assets/Scripts/Game.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Author: Vic
 [ExecuteInEditMode]
 public class Game : MonoBehaviour
 {
-
+    //Tiletag is the tag name which floor tiles have
     public string tileTag;
+    
+    //Size in the grid
     public float cellSize = 1;
+
     GameObject player;
+
+    //List of floor tiles
     GameObject[] tiles;
+
+    //Height of the player over the floor
     float playerHeight;
 
 
+    //This function is called to check wether floor tiles are next to the given coordinates, thus they are accessble
     public bool canMakeMovement(float x, float y)
     {
+        //We have to check every floor tile we have
         foreach (GameObject go in tiles)
         {
             if (go.GetComponent<GridTile>().movable(x, y)) return true;
@@ -28,20 +39,22 @@ public class Game : MonoBehaviour
     {
         player = GameObject.Find("Player");
         playerHeight = player.GetComponent<PlayerMovement>().height;
+
+        //Tiles will get every gameObject whose tag is "tileTag"
         tiles = GameObject.FindGameObjectsWithTag(tileTag);
         Debug.Log("Current Tiles in the scene: " + tiles.Length);
 
+        //There's only a starting point in our scene, we have to count how many of them we have
         int startingTileCounter = 0;
 
+        //This loop can be use to configure the scene
         foreach (GameObject go in tiles)
         {
+
+            //Moving player to the starting point
             if (go.GetComponent<GridTile>().startingTile)
             {
                 startingTileCounter++;
-
-                if (startingTileCounter > 1) {
-                    Debug.LogError("There are "+startingTileCounter+" starting Tiles");
-                }
 
                 Vector3 aux = go.transform.position;
                 aux.y = player.GetComponent<PlayerMovement>().height;
@@ -53,11 +66,17 @@ public class Game : MonoBehaviour
 
             
         }
+
+        if (startingTileCounter > 1)
+        {
+            Debug.LogError("There are " + startingTileCounter + " starting Tiles");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //This is executed only in play mode
         if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode) { 
             
         }
--- Very Black Knight/Assets/Scripts/GridTile.cs
+++ Very Black Knight/Assets/Scripts/GridTile.cs
@@ -9,9 +9,6 @@ public class GridTile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-
-
         gameObject.tag = GameObject.Find("GameController").GetComponent<Game>().getTileTag();
     }
 
